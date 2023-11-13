02e3f732d1f6e3113a1847ccf2aeb26059e36835
Nexitude <145889196+Nexitude@users.noreply.github.com>
2023-11-13 13:47:04 +0800

    Updated scratchcord with extra security updates
    UUID security locks, and randomized handshake keys

--- Scratchcord_Github-release/Scratchcord_github-release.py
+++ Scratchcord_Github-release/Scratchcord_github-release.py
@@ -3,17 +3,19 @@ from scratchcloud import CloudClient, CloudChange
 import threading
 import discord
 import asyncio
+import uuid
+import random
 
 # Constants
 LETTERS_FILE = r'Scratchcord\Letters\all_letters.txt'
+HANDSHAKE_ID = str(random.randrange(1000000000000000,10000000000000000))
+HANDSHAKE_VALIDATOR = str(uuid.uuid4())[:-3].upper()
 
-# Change these for your actual secrets
-TOKEN = 'Y0UR-T0K3N-H3R3'
+TOKEN = 'Bot token here'
 SCRATCH_USERNAME = 'Username here'
-SCRATCH_PASSWORD = 'SuperSecretPassword!'
-CHANNEL_ID = 12345678901234567890  # Channel id
-HANDSHAKE_ID = 1234567890987654321234567890  # Customize handshake ID and validator
-HANDSHAKE_VALIDATOR = '290318123123414812'
+SCRATCH_PASSWORD = 'Password here'
+SCRATCH_PROJECT_ID = 'Project ID here'
+CHANNEL_ID = 1020304050607080900  # Replace with actual chnnel
 
 handshake = ''
 status = 0
@@ -21,7 +23,7 @@ sender = ''
 history = []
 
 # Scratch init
-client = CloudClient('CastyEZ', '897885598')
+client = CloudClient(SCRATCH_USERNAME, SCRATCH_PROJECT_ID)
 letters = []
 
 
@@ -46,6 +48,8 @@ def encrypt(words: str):
 @client.event
 async def on_connect():
     print('Scratch hook connected')
+    print(f'Random UUID: {HANDSHAKE_VALIDATOR}XXX')
+    print(f'Key: {HANDSHAKE_ID}')
 
 
 @client.event
@@ -55,18 +59,23 @@ async def on_disconnect():
 
 @client.event
 async def on_message(cloud: CloudChange):
-    global status, sender
+    global status, sender, handshake
     if cloud.name == 'Status':
         status = int(cloud.value)
 
     if cloud.name == '__key':
+        print('Incoming validation')
         if cloud.value == HANDSHAKE_ID:
-            await client.set_cloud('__key', HANDSHAKE_VALIDATOR)
+            await client.set_cloud('__key', '1')
+            handshake = HANDSHAKE_VALIDATOR + sender[:3].upper()
+            print('Validation success')
+            print(f'Handshake validator: {handshake}')
+
 
     elif cloud.name == 'Sender':
         sender = decrypt(cloud.value)
 
-    elif cloud.name == 'Value' and handshake == HANDSHAKE_VALIDATOR:
+    elif cloud.name == 'Value' and handshake == HANDSHAKE_VALIDATOR + sender[:3].upper():
         if status == 1:  # Post message
             value = decrypt(cloud.value)
             print(f'Request by {sender} to post a message')
@@ -101,7 +110,7 @@ bot = discord.Client(intents=intents)
 
 async def send_message_to_channel(message):
     channel = bot.get_channel(CHANNEL_ID)
-    await channel.send(message)
+    await channel.send(f'(Scratch) {message}')
 
 
 @bot.event
@@ -112,7 +121,7 @@ async def on_message(message):
 
     if message.author != bot.user:
         content = [message.content async for message in channel.history(limit=1)][0]
-        formatted_message = f'{str(message.author)[:-2]}: {content}'
+        formatted_message = f'(Discord) {str(message.author)[:-2]}: {content}'
         print(formatted_message)
         await message.delete()
         await channel.send(formatted_message)
@@ -130,6 +139,6 @@ async def on_ready():
     print('Discord bot ready')
 
 # Main loop
-scratchthread = threading.Thread(target=client.run, args=('CastyLoz17#HateU',))
+scratchthread = threading.Thread(target=client.run, args=(SCRATCH_PASSWORD, ))
 scratchthread.start()
 bot.run(TOKEN)
